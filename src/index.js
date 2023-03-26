57a4540d4540e0496f74e6838c450cf0f326d42f
prikugayuma <128953529+prikugayuma@users.noreply.github.com>
2023-03-26 17:00:28 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -80,10 +80,10 @@
 // const func2 = (str) => str;
 // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
 
 /**
  * 分割代入
@@ -96,3 +96,16 @@ console.log(func3(10, 20));
 
 // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 // console.log(message1);
+
+// const { name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
