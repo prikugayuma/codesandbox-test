c14d2d50030a01586dc6c7a44aec2bb1995a573f
prikugayuma <128953529+prikugayuma@users.noreply.github.com>
2023-03-26 16:43:13 +0900

    アロー関数まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
-import "./styles.css";
+/**
+ *
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言"
+// console.log(val1)
+
+// let val2 = "let変数";
+// console.log(val2)
+
+// // // let変数は上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2)
+
+// // // let変数は再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数"
+// console.log(val3)
+
+// // const変数は上書き不可
+// val3 = "const変数を上書き";
+// console.log(val3);
+
+// const val3 = "const変数を再宣言"
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//  name:"ゆうま",
+//  age: 28,
+// };
+// val4.addres = "Tokyo";
+// val4.name = "たまい";
+// console.log(val4);
+
+//constで定義した配列の値を変更、追加出来る
+// const val5 = ['dog', 'cat','insect'];
+// val5[0] = "bird";
+// val5.push("monkey")
+// console.log(val5);
+
+/**
+ * 
+テンプレート文字列
+ */
+
+//  const name = "じゃけぇ";
+//  const age = 28;
+//「私の名前はじゃけぇです。年齢は28歳です。」
+
+//従来の方法
+//  const message1 = "私の名前は" + name + "です。年齢は" + age　+ "です";
+//  console.log(message1);
+
+//  //テンプレート文字列を用いた方法
+//  const message2 = `私の名前は${name}です。年齢は${age}です。`;
+//  console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str){
+// return str;
+// }
+// const func1 = function(str){
+//   return str;
+// }
+// console.log(func1("func1です"));
+
+// //新アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
